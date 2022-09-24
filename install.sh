7aa8a56479735bc25219d7f60790d3ef06e649ab
jxm2001 <2950794142@qq.com>
2022-09-25 04:14:06 +0800

    update nvim

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-read -p "Choose vim version to install(null/base/easy/coc): " version
+read -p "Choose vim version to install(null/base/easy/coc/nvim): " version
 if [ $version == "base" ]; then
 	cp ./baseVim/vimrc ~/.vimrc
 elif [ $version == "easy" ]; then
@@ -17,7 +17,7 @@ elif [ $version == "coc" ]; then
 		echo "Network error!"
 		exit 2
 	fi
-	clangd --version &> /dev/null && ctags --version &> /dev/null && node --version &> /dev/null
+	clangd --version &> /dev/null && ctags --version &> /dev/null && npm --version &> /dev/null
 	if [ $? -ne 0 ]; then
 		echo "Please run the following command to install the dependent environment"
 		clangd --version &> /dev/null
@@ -38,6 +38,34 @@ elif [ $version == "coc" ]; then
 		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 	cp ./cocVim/vimrc ~/.vimrc
 	cp ./cocVim/coc-settings.json ~/.vim/coc-settings.json
+elif [ $version == "nvim" ]; then
+	curl --connect-timeout 3 google.com &> /dev/null
+	if [ $? -ne 0 ]; then
+		echo "Network error!"
+		exit 2
+	fi
+	clangd --version &> /dev/null && ctags --version &> /dev/null && npm --version &> /dev/null
+	if [ $? -ne 0 ]; then
+		echo "Please run the following command to install the dependent environment"
+		clangd --version &> /dev/null
+		if [ $? -ne 0 ]; then
+			echo "sudo apt install clangd"
+		fi
+		ctags --version &> /dev/null
+		if [ $? -ne 0 ]; then
+			echo "sudo apt install universal-ctags"
+		fi
+		node --version &> /dev/null
+		if [ $? -ne 0 ]; then
+			echo "curl -sL install-node.vercel.app/lts | sudo bash"
+		fi
+		exit 2
+	fi
+	curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	mkdir -p ~/.config/nvim
+	cp ./cocVim/vimrc ~/.config/nvim/init.vim
+	cp ./cocVim/coc-settings.json ~/.config/nvim/coc-settings.json 
 elif [ $version != "null" ]; then
 	echo "Error vim version"
 	exit 1
