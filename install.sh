d2ff8960b9f512b9528621e83e015624a88cdccb
jxm2001 <2950794142@qq.com>
2022-12-11 22:48:54 +0800

    fix install sh bug

--- install.sh
+++ install.sh
@@ -8,8 +8,10 @@ elif [ $version == "easy" ]; then
 		echo "Network error!"
 		exit 2
 	fi
-	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	if [ ! -e ~/.vim/autoload/plug.vim ]; then
+		curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+			https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	fi
 	cp ./easyVim/vimrc ~/.vimrc
 elif [ $version == "coc" ]; then
 	curl --connect-timeout 3 google.com &> /dev/null
@@ -34,8 +36,10 @@ elif [ $version == "coc" ]; then
 		fi
 		exit 2
 	fi
-	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	if [ ! -e ~/.vim/autoload/plug.vim ]; then
+		curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+			https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	fi
 	cp ./cocVim/vimrc ~/.vimrc
 	cp ./cocVim/coc-settings.json ~/.vim/coc-settings.json
 elif [ $version == "nvim" ]; then
@@ -44,7 +48,7 @@ elif [ $version == "nvim" ]; then
 		echo "Network error!"
 		exit 2
 	fi
-	clangd --version &> /dev/null && ctags --version &> /dev/null && npm --version &> /dev/null && tree-sitter &> /dev/null
+	clangd --version &> /dev/null && ctags --version &> /dev/null && npm --version &> /dev/null && tree-sitter --version &> /dev/null
 	if [ $? -ne 0 ]; then
 		echo "Please run the following command to install the dependent environment"
 		clangd --version &> /dev/null
@@ -65,7 +69,9 @@ elif [ $version == "nvim" ]; then
 		fi
 		exit 2
 	fi
-	git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+	if [ ! -e ~/.local/share/nvim/site/pack/packer/start/packer.nvim ]; then
+		git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+	fi
 	mkdir -p ~/.config/nvim
 	cp ./neoVim/init.vim ~/.config/nvim/init.vim
 	cp ./neoVim/coc-settings.json ~/.config/nvim/coc-settings.json
@@ -83,7 +89,9 @@ elif [ $version == "normal" ]; then
 		echo "Network error!"
 		exit 2
 	fi
-	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+	if [ ! -e ~/.tmux/plugins/tpm ]; then
+		git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+	fi
 	cp ./tmux/normalTmux.conf ~/.tmux.conf
 elif [ $version != "null" ]; then
 	echo "Error tmux version"
