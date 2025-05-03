7e2d40033d431b8ac84b9b1506283d93ff42551e
jxm2001 <2950794142@qq.com>
2025-05-03 11:23:20 +0800

    rollback alpha icons

--- nvim_lsp/lua/plugins.lua
+++ nvim_lsp/lua/plugins.lua
@@ -10,11 +10,9 @@ return {
 	},
 	{
 		"goolord/alpha-nvim",
-		dependencies = { 'nvim-tree/nvim-web-devicons' },
+		dependencies = { 'echasnovski/mini.icons' },
 		config = function()
-			local startify = require("alpha.themes.startify")
-			startify.file_icons.provider = "devicons"
-			require("alpha").setup(startify.config)
+			require'alpha'.setup(require'alpha.themes.startify'.config)
 		end,
 	},
 	{
