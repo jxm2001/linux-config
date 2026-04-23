0093ac088ba5524754d0d95838a07a46a955964c
jxm2001 <jxm2001hpc@gmail.com>
2026-04-23 11:20:06 +0800

    Migration nvim-treesitter config to main branch

--- nvim_lsp/lua/plugins/nvim-treesitter.lua
+++ nvim_lsp/lua/plugins/nvim-treesitter.lua
@@ -1,19 +1,43 @@
 return{
 	{
 		"nvim-treesitter/nvim-treesitter",
-		branch = "master",
+		branch = "main",
 		build = ":TSUpdate",
-		config = function ()
-			local configs = require("nvim-treesitter.configs")
-			configs.setup({
-				ensure_installed = { "cpp", "c", "cuda", "cmake", "bash", "vim", "python",
-					"lua", "markdown", "markdown_inline", "fortran" },
-				sync_install = false,
-				highlight = { enable = true },
-				indent = { enable = true },
+		config = function()
+			local ts = require("nvim-treesitter")
+
+			local langs = {
+				"cpp",
+				"c",
+				"cuda",
+				"cmake",
+				"bash",
+				"vim",
+				"python",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"fortran",
+			}
+
+			ts.install(langs)
+
+			vim.api.nvim_create_autocmd("FileType", {
+				group = vim.api.nvim_create_augroup("my_treesitter", { clear = true }),
+				callback = function(args)
+					local lang = vim.treesitter.language.get_lang(vim.bo[args.buf].filetype)
+					if not lang or lang == "" then
+						return
+					end
+					pcall(vim.treesitter.start, args.buf, lang)
+					if vim.treesitter.query.get(lang, "indents") then
+						vim.bo[args.buf].indentexpr =
+							"v:lua.require'nvim-treesitter'.indentexpr()"
+					end
+				end,
 			})
 		end,
-		lazy = false
+		lazy = false,
 	},
 	{ 'HiPhish/rainbow-delimiters.nvim', lazy = false },
 }
